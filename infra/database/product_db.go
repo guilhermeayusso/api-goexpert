d694381ca7348fa38f31c73bdfb80c82b39148cd
Guilherme Ayusso Guimarães <guilherme@MacBook-Pro-de-Guilherme.local>
2025-05-24 20:28:37 -0300

    Criando product

---
+++ infra/database/product_db.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"githug.com/guilhermeayusso/api-goexpert/internal/entity"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *Product {
+	return &Product{DB: db}
+}
+
+func (p *Product) Create(product *entity.Product) error {
+	return p.DB.Create(product).Error
+}
+
+func (p *Product) FindByID(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := p.DB.First(&product, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (p *Product) Update(product *entity.Product) error {
+	_, err := p.FindByID(product.ID.String())
+	if err != nil {
+		return err
+	}
+	return p.DB.Save(product).Error
+}
+
+func (p *Product) Delete(id string) error {
+	product, err := p.FindByID(id)
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(product).Error
+}
