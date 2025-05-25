79eb16f3d121f29b2469f6e45c7cd939b36e1581
Guilherme Ayusso Guimarães <guilherme@MacBook-Pro-de-Guilherme.local>
2025-05-24 23:50:24 -0300

    Incluindo paginacao em produtos

--- infra/database/product_db.go
+++ infra/database/product_db.go
@@ -26,6 +26,24 @@ func (p *Product) FindByID(id string) (*entity.Product, error) {
 	return &product, nil
 }
 
+func (p *Product) FindAll(page, limit int, sort string) ([]*entity.Product, error) {
+	var products []*entity.Product
+	var err error
+
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+
+	if page != 0 && limit != 0 {
+		err = p.DB.Offset((page - 1) * limit).Limit(limit).Order("created_at " + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("created_at " + sort).Find(&products).Error
+	}
+
+	return products, err
+
+}
+
 func (p *Product) Update(product *entity.Product) error {
 	_, err := p.FindByID(product.ID.String())
 	if err != nil {
