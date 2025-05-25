df72dafa7584a6812af54a3f78eb6688cdad8b25
Guilherme Ayusso Guimarães <guilherme@MacBook-Pro-de-Guilherme.local>
2025-05-25 19:48:05 -0300

    finalizando testes de Product

--- infra/database/product_db_test.go
+++ infra/database/product_db_test.go
@@ -64,3 +64,71 @@ func TestFindAllProducts(t *testing.T) {
 	assert.Equal(t, products[0].Name, "Product 21")
 	assert.Equal(t, products[2].Name, "Product 23")
 }
+
+func TestFindProductByID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.0)
+
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	productFound, err := productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, product.ID.String(), productFound.ID.String())
+	assert.Equal(t, product.Name, productFound.Name)
+	assert.Equal(t, product.Price, productFound.Price)
+}
+
+func TestUpdateProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.0)
+
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	product.Name = "Product 2"
+	err = productDB.Update(product)
+	assert.NoError(t, err)
+
+	productFound, err := productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, product.ID.String(), productFound.ID.String())
+	assert.Equal(t, "Product 2", productFound.Name)
+	assert.Equal(t, product.Price, productFound.Price)
+}
+
+func TestDeleteProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.0)
+
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	err = productDB.Delete(product.ID.String())
+	assert.NoError(t, err)
+
+	productFound, err := productDB.FindByID(product.ID.String())
+	assert.Error(t, err)
+	assert.Nil(t, productFound)
+}
